b3d30d94b2513cfcafeb4ab7abb5fed4ad99edd4
Haley DeKeyser <hcd09@live.com>
2019-05-31 18:22:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ require_relative './course.rb'
 class Scraper
   
   def get_page
-    page = Nokogiri::HTML(open('https://learn-co-curriculum.github.io/site-for-scraping/courses'))
+    Nokogiri::HTML(open('https://learn-co-curriculum.github.io/site-for-scraping/courses'))
   end
   
   def get_courses
